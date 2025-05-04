d2c97fb4a399c23a0fd4b969fe46f686892a8c11
thinh.thaitri0409@gmail.com <thinh.thai09@hcmut.edu.vn>
2025-05-04 22:17:43 +0700

    done create post

--- src/app/(admin)/admin/posts/create/page.js
+++ src/app/(admin)/admin/posts/create/page.js
@@ -1,12 +1,62 @@
-export default function Dashboard() {
+'use client';
+
+export default function Page() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const titleInput = event.currentTarget.querySelector("#title");
+    const contentTextarea = event.currentTarget.querySelector("#content");
+
+    const title = titleInput ? titleInput.value : "";
+    const content = contentTextarea ? contentTextarea.value : "";
+
+    if (!title.trim()) {
+      alert("Hãy nhập tiêu đề bài viết trước khi đăng bài");
+      return;
+    } else if (!content.trim()) {
+      alert("Hãy nhập nội dung bài viết trước khi đăng bài");
+      return;
+    }
+    
+
+    try {
+      const data = {
+        title: title,
+        content: content
+      }
+      console.log(data);
+
+      const response = await fetch("/api/post/create-post", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        alert("Tạo bài viết mới thành công!");
+        titleInput.value = '';
+        contentTextarea.value = '';
+        // You might want to refresh the comments section here.
+      } else {
+        const errorData = await response.json();
+        alert(`Lỗi: ${errorData.message || 'Không thể gửi bình luận'}`);
+      }
+    } catch (e) {
+      alert(`Lỗi: ${e.message || "Đã có lỗi xảy ra"}`);
+      console.error("Error submitting comment:", e)
+    }
+  };
+
   return (
     <div className="m-4">
-      <h1 className="text-2xl">Tạo bài viết mới</h1>
-      <form className="flex flex-col">
+      <h1 className="text-2xl mb-6">Tạo bài viết mới</h1>
+      <form className="flex flex-col" onSubmit={handleSubmit}>
         <label className="ml-2 font-bold mb-2">Tiêu đề</label>
-        <input type="text" className="border-solid border-2 border-gray-400 rounded p-2 mb-2" />
+        <input type="text" id="title" name="title" className="border-solid border-2 border-gray-400 rounded p-2 mb-2" />
         <label className="ml-2 font-bold mb-2">Nội dung bài viết</label>
-        <textarea className="border-solid border-2 border-gray-400 rounded p-2 mb-2" />
+        <textarea id="content" name="content" className="border-solid border-2 border-gray-400 rounded p-2 mb-2" />
         <label className="ml-2 font-bold mb-2">Thêm hình ảnh: </label>
 
         <button type="submit" href="posts/create" className="w-fit px-5 py-3 rounded-xl my-2 bg-[#435ebe] font-bold text-white hover:bg-blue-800">Đăng bài viết</button>
--- src/app/(admin)/admin/posts/page.js
+++ src/app/(admin)/admin/posts/page.js
@@ -1,7 +1,7 @@
 export default function Dashboard() {
   return (
     <div className="m-4">
-      <h1 className="text-2xl mb-10">Bài viết</h1>
+      <h1 className="text-2xl mb-6">Bài viết</h1>
       <a href="posts/create" className="px-5 py-3 rounded-xl my-2 bg-[#435ebe] text-white hover:bg-blue-800">Create a post</a>
     </div>
   );
